7473c54e916e0c4e20db1a45d8e4e2c57c84927a
Peter Vertenten <peter.vertenten@gmail.com>
2015-11-12 17:06:11 -0500

    adding stub custom.js

---
+++ js/custom.js
@@ -0,0 +1,22 @@
+// CustomJsSIPSettings = {
+//   uri: null,
+//   password: null,
+//   ws_servers: null,
+//   display_name: null,
+//   authorization_user: null,
+//   register: null,
+//   register_expires: null,
+//   no_answer_timeout: null,
+//   use_preloaded_route: null,
+//   connection_recovery_min_interval: null,
+//   connection_recovery_max_interval: null,
+//   hack_via_tcp: null,
+//   hack_ip_in_contact: null
+// };
+
+
+// Configure in Settings some parameters.
+//
+// Settings = {
+//   videoDisabledByDefault: true
+// };
